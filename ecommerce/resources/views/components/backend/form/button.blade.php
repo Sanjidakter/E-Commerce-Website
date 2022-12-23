7a7b07d6edc94cb3c8366da897b514d09474f0b1
Sanjida Akter <sanji2601@gmail.com>
2022-12-23 13:49:36 +0600

    initial commit

--- ecommerce/resources/views/components/backend/form/button.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- @props(['name','type'=>'submit'])
-<button type={{$type}} class="btn btn-primary">{{$name}}</button> --}}
-
-<button type="submit" class="btn btn-primary">{{$slot}}</button>
--- ecommerce/resources/views/components/backend/partials/elements/message.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['message'])
-@if($message)
-            <div class="aleart alert-success">
-                <span class="close" data-dismiss="alert">
-                </span>
-                <strong>{{$message}}</strong>
-            </div>
-@endif
